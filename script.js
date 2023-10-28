5392dae7cda61efb1dccd3a7607e703697c55572
Jobgetz <jobgetzinger@gmail.com>
2023-10-28 15:07:11 -0400

    puuush

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const axios = require('axios');
+
 const apiKey = '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';
 const apiURL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
 
 async function breakDownText(prompt) {
+    // input a prompt into chatgpt and recieve categories.
     const response = await axios.post(apiURL,{
         prompt: prompt,
         max_tokens: 20,
@@ -10,5 +11,8 @@ async function breakDownText(prompt) {
         headers: {
             'Authorization': `Bearer ${apiKey}`,
         }
-    })
+    });
+    // recieve categories
+    category = response.data.choices[0].text;
 }
+console.log(output);
